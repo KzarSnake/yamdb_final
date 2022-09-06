298f76e6b0d3a369535f575a531d1f2fd0407c4b
Денис Свашенко <dsvash@yandex.ru>
2022-09-07 02:29:17 +0300

    Fix workflow 5

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -109,7 +109,7 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-        # очь странные дела1
+        # очь странные дела
 
 
 class SignUpView(views.APIView):
