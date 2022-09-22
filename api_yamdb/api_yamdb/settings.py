8fa2235c73e840cd0641c146c3129bde0c53e53a
Денис Свашенко <dsvash@yandex.ru>
2022-09-22 23:27:27 +0300

    Return to prev iteration

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
